8488efd747c75ed8553cc7d97942db322e4be9ed
zovikk <zovik1999zz@gmail.com>
2022-11-26 11:46:49 +0700

    Lesson_1_3: Начал изучать работу с presentation-слоем. Ошибка: в этом слое есть отсылка к data-слою, о котором presentation не должен знать.

---
+++ app/src/main/java/com/example/shoplist/presentation/MainViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.shoplist.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.shoplist.data.ShopListRepositoryImpl
+import com.example.shoplist.domain.DeleteShopItemUseCase
+import com.example.shoplist.domain.EditShopItemUseCase
+import com.example.shoplist.domain.GetShopListUseCase
+import com.example.shoplist.domain.ShopItem
+
+class MainViewModel: ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = MutableLiveData<List<ShopItem>>()
+
+    fun getShopList() {
+        val list = getShopListUseCase.getShopList()
+        shopList.value = list
+    }
+
+    fun deleteShopItem(shopItem: ShopItem){
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+        getShopList()
+    }
+
+    fun changeEnableState(shopItem: ShopItem){
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newItem)
+        getShopList()
+    }
+}
